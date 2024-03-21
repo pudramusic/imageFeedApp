9c37dcb3417d6a62fda0dda0016cf8a9f54be394
pudramusic <flyandseeru@gmail.com>
2024-03-21 20:32:11 +0300

    Добавлен; - Navigator Bar - class SingleImageViewController

---
+++ imageFeedApp/ImagesList/SingleImageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SingleImageViewController.swift
+//  imageFeedApp
+//
+//  Created by Yo on 21/3/24.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    
+}
