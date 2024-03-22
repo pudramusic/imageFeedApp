bbd4306d72741ee89ccb4c21061a3f39081afd80
pudramusic <flyandseeru@gmail.com>
2024-03-22 16:08:11 +0300

    - Переходы между ImageList и SingleImage - ViewDidLoad честное решение

--- imageFeedApp/ImagesList/ImageListViewController.swift
+++ imageFeedApp/ImagesList/ImageListViewController.swift
@@ -16,6 +16,7 @@ final class ImageListViewController: UIViewController {
     // MARK: - Properties
         
     private let photoName: [String] = Array(0..<20).map{ "\($0)"} // создаем массив с картинками с названиями от 0 до 19
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
     
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -28,25 +29,43 @@ final class ImageListViewController: UIViewController {
     
     override func viewDidLoad() {
         
-        tableView.dataSource = self // устанавливаем связь с DataSourse, где self это контроллер, который должен выступать как датаСорс
-        tableView.delegate = self // устанавливаем связь с Delegate, где self это контроллер, который должен выступать как делегат
+        tableView.dataSource = self // устанавливаем связь с DataSourse
+        tableView.delegate = self // устанавливаем связь с Delegate
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0) // создаем отступы содержимого ячейки
 
         super.viewDidLoad()
         
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) { // метод чтобы открыть выбранную картинку
+        if segue.identifier == showSingleImageSegueIdentifier { // делаем проверку сегвея куда будем делать переход
+            guard
+                let viewController = segue.destination as? SingleImageViewController, // преобразуем segue
+                let indexPath = sender as? IndexPath // преобразуем sender
+            else {
+                assertionFailure("Invalid segue destination")
+                return
+            }
+            
+            let image = UIImage(named: photoName[indexPath.row]) // порлучаем верную картинку по индексу
+            viewController.image = image // передаем верную картинку контроллеру
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
     }
 }
 
     // MARK: - Extension
 
 extension ImageListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { // отвечает за действия, которые будут выполнены после тапа
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { // отвечает за действия после тапа по ячейке
         tableView.deselectRow(at: indexPath, animated: true) // убираем подсветку после тапа
+        performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat { // задаем высоту ячейке, т.к ячейки имеют «динамическую» высоту — то есть зависят от размеров фото, которые они отображают. Метод возвращает высоту строки
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat { // задаем высоту ячейке, т.к ячейки имеют «динамическую» высоту
         guard let image = UIImage(named: photoName[indexPath.row]) else { // проверяем наличие изображения
-            return 0 // если изображения нет, то возвращаем высоту строки равную 0
+            return 0
         }
         let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16) // создаем экземпляр который содержит отступы от краев изображения
         let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right // вычисляем ширину изображения внутри ячейки путем вычитания левого и правого отступов
@@ -63,10 +82,10 @@ extension ImageListViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { // в этом методе создаем ячейку, наполняем данными и передаем таблице
-        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath) // метод который возвращает ячейку по идентификатору
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
         
         guard let imageListCell = cell as? ImagesListCell else { // чтобы работать с ячейкой как с экз класса ImageListCell проводим приведение типов
-            return UITableViewCell() // возвращаем ячейку
+            return UITableViewCell()
         }
 
         configCell(for: imageListCell, with: indexPath) // вызываем метод конфигурации ячейки
@@ -76,7 +95,7 @@ extension ImageListViewController: UITableViewDataSource {
 
 extension ImageListViewController {
     func configCell(for cell: ImagesListCell, with indexPath: IndexPath) { // метод конфигурации ячейки
-        guard let image = UIImage(named: photoName[indexPath.row]) else { // проверяем наличие изображения в масиве по индексу
+        guard let image = UIImage(named: photoName[indexPath.row]) else {
             return
         }
         cell.cellImage.image = image // устанавливаем изображение в ячейку
@@ -86,8 +105,8 @@ extension ImageListViewController {
         cell.dateLabel.text = dateFormatter.string(from: Date()) // устанавливаем дату в ячейку
 
         let isLiked = indexPath.row % 2 == 0 // проверяем нравится ли картинка.
-        let likeImage = isLiked ? UIImage(named: "active") : UIImage(named: "noActive") // данная переменная содержит ответ для кнопки в зависимости понравилась картинка или нет
-        cell.likeButton.setImage(likeImage, for: .normal) // устанавливаем значение
+        let likeImage = isLiked ? UIImage(named: "active") : UIImage(named: "noActive")
+        cell.likeButton.setImage(likeImage, for: .normal)
     }
 }
 
--- imageFeedApp/ImagesList/SingleImageViewController.swift
+++ imageFeedApp/ImagesList/SingleImageViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 final class SingleImageViewController: UIViewController {
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else { return } // проверяем был ли раньше загружен view
+            imageView.image = image 
+        }
+    }
     
     // MARK: - Lifecycle
     
     @IBOutlet var imageView: UIImageView!
     
+    // MARK: - Override
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
 }
