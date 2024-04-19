64d05c0198831f802db61c6383845b833e393091
pudramusic <flyandseeru@gmail.com>
2024-04-19 12:58:14 +0300

    Повторная сдача на ревью

--- imageFeedApp/Model/Token/OAuthTokenResponseBody.swift
+++ imageFeedApp/Model/Token/OAuthTokenResponseBody.swift
@@ -2,18 +2,6 @@
 
 import Foundation
 
-//struct OAuthTokenResponseBody: Codable {
-//    let accessToken: String?
-//    let tokenType: String
-//    let scope: String
-//    let createdAt: Int
-//    enum CodingKeys: String, CodingKey {
-//        case accessToken = "access_token"
-//        case tokenType = "token_type"
-//        case scope = "scope"
-//        case createdAt = "created_at"
-//    }
-//}
 struct OAuthTokenResponseBody: Codable {
     enum CodingKeys: String, CodingKey {
         case accessToken = "access_token"
