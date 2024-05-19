2db2bd75435ebb919cfd88dc383adf10fe65a8ac
pudramusic <flyandseeru@gmail.com>
2024-05-19 12:17:39 +0300

    no message

--- imageFeedApp/ImageFeedAppTests/ImagesListServiceTests.swift
+++ imageFeedApp/ImageFeedAppTests/ImagesListServiceTests.swift
@@ -3,25 +3,26 @@ import XCTest
 @testable import imageFeedApp
 
 final class imageFeedAppTests: XCTestCase {
-
-
+    
+    
     func testExample() throws {
-
+        
     }
     func testFetchPhotos() {
         let service = ImagesListService.shared
-            
-            let expectation = self.expectation(description: "Wait for Notification")
-            NotificationCenter.default.addObserver(
-                forName: ImagesListService.didChangeNotification,
-                object: nil,
-                queue: .main) { _ in
-                    expectation.fulfill()
-                }
-            
-            service.fetchPhotosNextPage()
-            wait(for: [expectation], timeout: 10)
-            
-            XCTAssertEqual(service.photos.count, 10)
-        }
+        
+        let expectation = self.expectation(description: "Wait for Notification")
+        NotificationCenter.default.addObserver(
+            forName: ImagesListService.didChangeNotification,
+            object: nil,
+            queue: .main) { _ in
+                expectation.fulfill()
+            }
+        
+        service.fetchPhotosNextPage()
+        wait(for: [expectation], timeout: 10)
+        
+        XCTAssertEqual(service.photos.count, 10)
+        service.fetchPhotosNextPage()
+    }
 }
