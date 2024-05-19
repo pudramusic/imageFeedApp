781790b63b422a9082caa5d3c66e8584e116e047
pudramusic <flyandseeru@gmail.com>
2024-05-19 12:11:27 +0300

    Добавлен тест testFetchPhotos

---
+++ imageFeedApp/ImageFeedAppTests/ImagesListServiceTests.swift
@@ -0,0 +1,27 @@
+
+import XCTest
+@testable import imageFeedApp
+
+final class imageFeedAppTests: XCTestCase {
+
+
+    func testExample() throws {
+
+    }
+    func testFetchPhotos() {
+        let service = ImagesListService.shared
+            
+            let expectation = self.expectation(description: "Wait for Notification")
+            NotificationCenter.default.addObserver(
+                forName: ImagesListService.didChangeNotification,
+                object: nil,
+                queue: .main) { _ in
+                    expectation.fulfill()
+                }
+            
+            service.fetchPhotosNextPage()
+            wait(for: [expectation], timeout: 10)
+            
+            XCTAssertEqual(service.photos.count, 10)
+        }
+}
